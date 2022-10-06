59c202b06fc78eade534636d53a9baf10bf0f7ae
NoDiggity.Dexter <dimonshinkarenko2001@gmail.com>
2022-10-06 11:27:22 +0300

    created a design for the movement of the figures

--- src/components/board/board-component.tsx
+++ src/components/board/board-component.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import { useState, Fragment, useEffect } from 'react';
 import { Board } from '../../modals/Board';
+import { Cell } from '../../modals/Cell';
 import { CellComponent } from '../cell';
 
 interface Props {
@@ -8,16 +9,52 @@ interface Props {
 }
 
 export const BoardComponent = ({ board, setBoard }: Props) => {
-	console.log(board.cells);
+	const [selectedCell, setSelectedCell] = useState<Cell | null>(null);
+
+	function onCellClick(cell: Cell) {
+		if (
+			selectedCell &&
+			selectedCell !== cell &&
+			selectedCell.figure?.canMove(cell)
+		) {
+			selectedCell.moveFigure(cell);
+			setSelectedCell(null);
+			updateBoard();
+		} else {
+			setSelectedCell(cell);
+		}
+	}
+
+	function highlightCells() {
+		board.highlightCells(selectedCell);
+		updateBoard();
+	}
+
+	function updateBoard() {
+		const newBoard = board.getCopyBoard();
+		setBoard(newBoard);
+	}
+
+	useEffect(() => {
+		highlightCells();
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [selectedCell]);
 
 	return (
 		<div className='board'>
 			{board.cells.map((row, index) => (
-				<React.Fragment key={index}>
+				<Fragment key={index}>
 					{row.map((cell) => (
-						<CellComponent key={cell.id} cell={cell} />
+						<CellComponent
+							click={onCellClick}
+							key={cell.id}
+							cell={cell}
+							selected={
+								cell.x === selectedCell?.x && cell.y === selectedCell?.y
+							}
+						/>
 					))}
-				</React.Fragment>
+				</Fragment>
 			))}
 		</div>
 	);
--- src/components/cell/cell-component.tsx
+++ src/components/cell/cell-component.tsx
@@ -3,11 +3,19 @@ import { Cell } from '../../modals/Cell';
 
 interface Props {
 	cell: Cell;
+	selected: boolean;
+	click: (cell: Cell) => void;
 }
 
-export const CellComponent = ({ cell }: Props) => {
+export const CellComponent = ({ cell, selected, click }: Props) => {
 	return (
-		<div className={`cell ${cell.color}`}>
+		<div
+			onClick={() => click(cell)}
+			className={`cell ${cell.color} ${selected ? 'selected' : ''} ${
+				cell.available && cell.figure ? 'cell__attack' : ''
+			}`}
+		>
+			{cell.available && !cell.figure && <div className='available' />}
 			{cell.figure?.logo && <img src={cell.figure.logo} alt='' />}
 		</div>
 	);
